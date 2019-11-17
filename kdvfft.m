5fd2d2f71eda73fa69f7dafc90ec6c1a8aee233d
juan <jgrethe@itba.edu.ar>
2019-11-17 16:34:22 -0300

    kdv with orders working

--- kdvfft.m
+++ kdvfft.m
@@ -1,7 +1,12 @@
+
 %lie trotter
 %Kdv Dos_solitones
+function kdvfft(kdvOrder)
+if(mod(kdvOrder,2) ~= 0)
+    disp("Order must be pair");
+    return;
+end
 
-clear all
 clc
 set(gca,'FontSize',8)
 set(gca,'LineWidth',2)
@@ -28,53 +33,33 @@ drawnow
 
 tmax = 1.5; nplt = floor((tmax/100)/delta_t); nmax = round(tmax/delta_t);
 udata = u.'; tdata = 0;
-U1 = fft(u);
-U2 = fft(u);
-U3 = fft(u);
-U4 = fft(u);
+for i=1:1:kdvOrder
+    U{i} = fft(u);
+end
 
 % orders 2 , 4 , 6
 orders = {[-1/6,2/3], [1/90,-2/9,0,32/45], [-1/1680,1/15,-27/80,0,0,27/35]};
-order = orders{1,2};
+order = orders{1,kdvOrder/2};
 for n = 1:nmax-40000
+    
     t = n*delta_t;
-    
-    % lineal
-    U1 = U1.*exp(1i*k.^3*delta_t);
-    % no lineal
-    U1 = U1  - (3i*k*delta_t).*fft((real(ifft(U1))).^2);
-    
-    % no lineal
-    U2 = U2 - (3i*k*delta_t).*fft((real(ifft(U2))).^2);
-    % lineal
-    U2 = U2.*exp(1i*k.^3*delta_t);
-    
-    % lineal
-    U3 = U3.*exp(1i*k.^3*delta_t/2);
-    % no lineal
-    U3 = U3  - (3i*k*delta_t/2).*fft((real(ifft(U3))).^2);
-    % lineal
-    U3 = U3.*exp(1i*k.^3*delta_t/2);
-    % no lineal
-    U3 = U3  - (3i*k*delta_t/2).*fft((real(ifft(U3))).^2);
-    
-    % no lineal
-    U4 = U4 - (3i*k*delta_t/2).*fft((real(ifft(U4))).^2);
-    % lineal
-    U4 = U4.*exp(1i*k.^3*delta_t/2);
-    % no lineal
-    U4 = U4 - (3i*k*delta_t/2).*fft((real(ifft(U4))).^2);
-    % lineal
-    U4 = U4.*exp(1i*k.^3*delta_t/2);
-    
-    U = U1*order(1) + U2*order(2)+U3*order(3)+U4*order(4);
+    div = 1;
+    for i = 1:2:kdvOrder
+        U{i} = linearNonLinear(delta_t/div,k,U{i},i+1);
+        U{i+1} = nonLinearLinear(delta_t/div,k,U{i+1},i+1);
+        div = div + 1;
+    end
+    retU = 0;
+    for i=1:1:kdvOrder
+        retU = retU + 2 * order(i) * U{i};
+    end
     
     if mod(n,nplt) == 0
-        u = real(ifft(U));
+        u = real(ifft(retU));
         udata = [udata u.']; tdata = [tdata t];
         if mod(n,4*nplt) == 0
             plot(x,u,'LineWidth',2)
-            axis([-10 10 -5 10])
+            axis([-10 10 0 10])
             xlabel('x')
             ylabel('u')
             text(6,9,['t = ',num2str(t,'%1.2f')],'FontSize',10)
@@ -88,3 +73,33 @@ figure
 waterfall(x,tdata(1:4:end),udata(:,1:4:end)')
 xlabel x, ylabel t, axis([-10 10 0 tmax 0 10]), grid off
 zlabel u
+end
+
+function ret=linear(delta_t,k,U)
+ret = U.*exp(1i*k.^3*delta_t);
+end
+
+function ret=nonlinear(delta_t,k,U)
+ret = U - (3i*k*delta_t).*fft((real(ifft(U))).^2);
+end
+
+function ret=linearNonLinear(delta_t,k,U,order)
+ret = U;
+for i=1:1:order/2
+    ret = linear(delta_t,k,ret);
+    ret = nonlinear(delta_t,k,ret);
+end
+end
+
+function ret=nonLinearLinear(delta_t,k,U,order)
+ret = U;
+for i=1:1:order/2
+    ret = nonlinear(delta_t,k,ret);
+    ret = linear(delta_t,k,ret);
+end
+end
+
+
+
+
+
