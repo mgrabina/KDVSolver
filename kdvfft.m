e1997e74c6c1884ec9a21c4896bde4d053b6ea36
juan <jgrethe@itba.edu.ar>
2019-11-17 16:56:46 -0300

    merge linear and non linear calculation to a single function

--- kdvfft.m
+++ kdvfft.m
@@ -39,15 +39,14 @@ end
 
 % orders 2 , 4 , 6
 orders = {[-1/6,2/3], [1/90,-2/9,0,32/45], [-1/1680,1/15,-27/80,0,0,27/35]};
+map = [2, 2, 4, 4, 6, 6];
 order = orders{1,kdvOrder/2};
 for n = 1:nmax-40000
     
     t = n*delta_t;
-    div = 1;
-    for i = 1:2:kdvOrder
-        U{i} = linearNonLinear(delta_t/div,k,U{i},i+1);
-        U{i+1} = nonLinearLinear(delta_t/div,k,U{i+1},i+1);
-        div = div + 1;
+    
+    for i = 1:1:kdvOrder
+        U{i} = calculateOrder(delta_t/ceil(i/2),k,U{i},map(i),i);
     end
     retU = 0;
     for i=1:1:kdvOrder
@@ -83,22 +82,20 @@ function ret=nonlinear(delta_t,k,U)
 ret = U - (3i*k*delta_t).*fft((real(ifft(U))).^2);
 end
 
-function ret=linearNonLinear(delta_t,k,U,order)
+function ret=calculateOrder(delta_t,k,U,order,index)
 ret = U;
-for i=1:1:order/2
-    ret = linear(delta_t,k,ret);
-    ret = nonlinear(delta_t,k,ret);
+if(mod(index,2)==0)
+    for i=1:1:order/2
+        ret = nonlinear(delta_t,k,ret);
+        ret = linear(delta_t,k,ret);
+    end
+else
+    for i=1:1:order/2
+        ret = linear(delta_t,k,ret);
+        ret = nonlinear(delta_t,k,ret);
+    end
 end
 end
-
-function ret=nonLinearLinear(delta_t,k,U,order)
-ret = U;
-for i=1:1:order/2
-    ret = nonlinear(delta_t,k,ret);
-    ret = linear(delta_t,k,ret);
-end
-end
-
 
 
 
