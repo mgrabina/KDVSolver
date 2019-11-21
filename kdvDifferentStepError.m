da645b5eb040e42c566fa7dcfca2a6a5e9a690d8
juan <jgrethe@itba.edu.ar>
2019-11-20 21:55:27 -0300

    compare different step against a mayor order

--- kdvDifferentStepError.m
+++ kdvDifferentStepError.m
@@ -1,13 +1,9 @@
 
 %Kdv Dos_solitones
 % compares step1 = 0.4/256^² with step 2 = (0.4/256^²) * 1 / stepDifference
-% in order 'kdvOrder'
-function kdvDifferentStepError(kdvOrder,stepDifference)
-if(mod(kdvOrder,2) ~= 0)
-    disp("Order must be pair");
-    return;
-end
-
+% in order '4' againts order 6
+function kdvDifferentStepError(stepDifference)
+kdvOrder = 4;
 clc
 set(gca,'FontSize',8)
 set(gca,'LineWidth',2)
@@ -44,10 +40,16 @@ for i=1:1:kdvOrder
     U2{i} = fft(u);
 end
 
+
+for i=1:1:6
+    U3{i} = fft(u);
+end
+
 % orders 2 , 4 , 6
 orders = {[-1/6,2/3], [1/90,-2/9,0,32/45], [-1/1680,1/15,-27/80,0,0,27/35]};
 map = [2, 2, 4, 4, 6, 6];
 order = orders{1,kdvOrder/2};
+order2 = orders{1,3};
 time = 1;
 for n = 1:nmax-40000
     
@@ -61,7 +63,18 @@ for n = 1:nmax-40000
         retU = retU + 2 * order(i) * U{i};
     end
     
+    
+    % step 1 order 6
+    retU3 = 0;
+    for i = 1:1:6
+        U3{i} = calculateOrder(delta_t/ceil(i/2),k,U3{i},map(i),i);
+    end
+    for i=1:1:6
+        retU3 = retU3 + 2 * order2(i) * U3{i};
+    end
+    
     % step 2
+    retU2 = 0;
     for z = 1:stepDifference
         for i = 1:1:kdvOrder
             U2{i} = calculateOrder(delta_t2/ceil(i/2),k,U2{i},map(i),i);
@@ -75,20 +88,26 @@ for n = 1:nmax-40000
     if mod(n,nplt) == 0
         u = real(ifft(retU));
         u2 = real(ifft(retU2));
+        u3 = real(ifft(retU3));
         udata = [udata u.']; tdata = [tdata t];
         if mod(n,4*nplt) == 0
             subplot(1,2,1)
             t2(time) = t;
-            error(time) = mean(abs(u2 - u));
+            error1(time) = mean(abs(u3 - u));
+            error2(time) = mean(abs(u3 - u2));
             plot(x,u,'LineWidth',2)
             axis([-10 10 0 10])
             xlabel('x')
             ylabel('u')
             text(6,9,['t = ',num2str(t,'%1.2f')],'FontSize',10)
             subplot(1,2,2)
-            plot(t2,error);
+            plot(t2,error1,"b*");
+            hold on;
+            plot(t2,error2,"b-");
             xlabel('time[s]')
+            legend('Decremented step')
             ylabel('Mean Error')
+            hold off;
             time = time + 1;
             drawnow
         end
